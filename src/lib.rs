915a3d389be3345315621e61e851b67cb42d0c3e
Phillip Davis <phdavis1027@gmail.com>
2024-04-14 14:48:32 -0400

    wipping derived serialization

--- src/lib.rs
+++ src/lib.rs
@@ -50,14 +50,151 @@ pub fn rule(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 RuleParamType::String => quote! { String },
             };
             quote! {
-                #param_name: #param_type,
+                pub #param_name: #param_type,
             }
         })
         .collect::<Vec<_>>();
 
+    let input_struct_param_labels = input
+        .params
+        .iter()
+        .map(|param| {
+            let param_name = Ident::new(&param.label.value(), param.label.span());
+            quote! {
+                #param_name,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let rule_body = LitStr::new(
+        format!(
+            r##"
+    @external
+    {} {{
+        {}        
+    }}
+    "##,
+            input.name.value(),
+            input.body.value()
+        )
+        .as_str(),
+        input.body.span(),
+    );
+
     let out = quote! {
+        #[derive(Debug)]
         pub struct #input_struct_name {
             #(#input_struct_params)*
+            pub addr: ::std::net::SocketAddr,
+            pub rods_zone: String,
+            pub instance: Option<String>,
+        }
+
+
+        impl packe::bosd::Serialiazable for #input_struct_name { }
+        impl packe::bosd::xml::XMLSerializable for #input_struct_name {
+            fn to_xml(&self, sink: &mut Vec<u8>)
+                -> std::result::Result<usize, rods_prot_msg::error::errors::IrodsError>
+            {
+                use packe::{tag, tag_fmt};
+                use ::std::io::Write;
+
+                let mut cursor = ::std::io::Cursor::new(sink);
+                let mut writer = ::quick_xml::Writer::new(&mut cursor);
+
+                writer.write_event(::quick_xml::events::Event::Start(
+                        ::quick_xml::events::BytesStart::new("ExecMyRuleInp_PI")))?;
+
+                writer.write_event(::quick_xml::events::Event::Start(
+                    ::quick_xml::events::BytesStart::new("myRule")))?;
+                writer.write_event(::quick_xml::events::Event::Text(
+                    ::quick_xml::events::BytesText::new(#rule_body)))?;
+                writer.write_event(::quick_xml::events::Event::End(
+                    ::quick_xml::events::BytesEnd::new("myRule")))?;
+
+
+                writer.write_event(::quick_xml::events::Event::Start(
+                        ::quick_xml::events::BytesStart::new("RHostAddr_PI")))?;
+                writer.write_event(::quick_xml::events::Event::Start(
+                        ::quick_xml::events::BytesStart::new("hostAddr")))?;
+                ::std::write!(writer.get_mut(), "{}", self.addr.ip())?;
+                writer.write_event(::quick_xml::events::Event::End(
+                        ::quick_xml::events::BytesEnd::new("hostAddr")))?;
+
+                writer.write_event(::quick_xml::events::Event::Start(
+                        ::quick_xml::events::BytesStart::new("rodsZone")))?;
+                writer.write_event(::quick_xml::events::Event::Text(
+                    ::quick_xml::events::BytesText::new(self.rods_zone.as_str())))?;
+                writer.write_event(::quick_xml::events::Event::End(
+                        ::quick_xml::events::BytesEnd::new("rodsZone")))?;
+
+                writer.write_event(::quick_xml::events::Event::Start(
+                        ::quick_xml::events::BytesStart::new("port")))?;
+                ::std::write!(writer.get_mut(), "{}", self.addr.port())?;
+                writer.write_event(::quick_xml::events::Event::End(
+                        ::quick_xml::events::BytesEnd::new("port")))?;
+
+                writer.write_event(::quick_xml::events::Event::Start(
+                        ::quick_xml::events::BytesStart::new("dummyInt")))?;
+                writer.write_event(::quick_xml::events::Event::Text(
+                    ::quick_xml::events::BytesText::new("0")))?;
+                writer.write_event(::quick_xml::events::Event::End(
+                        ::quick_xml::events::BytesEnd::new("dummyInt")))?;
+
+                writer.write_event(::quick_xml::events::Event::End(
+                        ::quick_xml::events::BytesEnd::new("RHostAddr_PI")))?;
+
+                if let Some(inst) = &self.instance {
+                    writer.write_event(::quick_xml::events::Event::Start(
+                            ::quick_xml::events::BytesStart::new("KeyValPair_PI")))?;
+                    writer.write_event(::quick_xml::events::Event::Start(
+                            ::quick_xml::events::BytesStart::new("ssLen")))?;
+                    writer.write_event(::quick_xml::events::Event::Text(
+                        ::quick_xml::events::BytesText::new("1")))?;
+                    writer.write_event(::quick_xml::events::Event::End(
+                            ::quick_xml::events::BytesEnd::new("ssLen")))?;
+                    writer.write_event(::quick_xml::events::Event::Start(
+                            ::quick_xml::events::BytesStart::new("keyWord")))?;
+                    writer.write_event(::quick_xml::events::Event::Text(
+                        ::quick_xml::events::BytesText::new("instance_name")))?;
+                    writer.write_event(::quick_xml::events::Event::End(
+                            ::quick_xml::events::BytesEnd::new("keyWord")))?;
+                    writer.write_event(::quick_xml::events::Event::Start(
+                            ::quick_xml::events::BytesStart::new("svalue")))?;
+                    writer.write_event(::quick_xml::events::Event::Text(
+                        ::quick_xml::events::BytesText::new(&self.instance)))?;
+                    writer.write_event(::quick_xml::events::Event::End(
+                            ::quick_xml::events::BytesEnd::new("svalue")))?;
+                    writer.write_event(::quick_xml::events::Event::End(
+                            ::quick_xml::events::BytesEnd::new("KeyValPair_PI")))?;
+                } else {
+                    writer.write_event(::quick_xml::events::Event::Start(
+                            ::quick_xml::events::BytesStart::new("KeyValPair_PI")))?;
+                    writer.write_event(::quick_xml::events::Event::Start(
+                            ::quick_xml::events::BytesStart::new("ssLen")))?;
+                    writer.write_event(::quick_xml::events::Event::Text(
+                        ::quick_xml::events::BytesText::new("0")))?;
+                    writer.write_event(::quick_xml::events::Event::End(
+                            ::quick_xml::events::BytesEnd::new("ssLen")))?;
+                    writer.write_event(::quick_xml::events::Event::End(
+                            ::quick_xml::events::BytesEnd::new("KeyValPair_PI")))?;
+                }
+
+
+                writer.write_event(::quick_xml::events::Event::Start(
+                        ::quick_xml::events::BytesStart::new("MsParamArray_PI")))?;
+
+
+                writer.write_event(::quick_xml::events::Event::End(
+                        ::quick_xml::events::BytesEnd::new("MsParamArray_PI")))?;
+
+
+                writer.write_event(::quick_xml::events::Event::End(
+                        ::quick_xml::events::BytesEnd::new("ExecMyRuleInp_PI")))?;
+
+
+                Ok(cursor.position() as usize)
+            }
         }
     };
 
